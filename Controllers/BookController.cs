7070f1de7ecea7481d86edc0b2dbb48f01a9d38c
Jean Carlos De Meira <jean.meira10@hotmail.com>
2024-01-17 21:32:36 -0300

    feat: add getById

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -17,6 +17,17 @@ namespace books_api.Controllers
         public async Task<List<Book>> Get() =>
         await _booksService.GetAsync();
 
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Book>> Get(string id)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+                return NotFound();
+
+            return book;
+        }
+
         [HttpPost]
         public async Task<IActionResult> Post(Book newBook)
         {
